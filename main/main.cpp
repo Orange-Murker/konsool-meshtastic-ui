ebfb13842546ee9fa8cc55675de803d2e2dcdc29
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2025-05-23 20:38:43 +0200

    chore: remove unnecessary variables

--- main/main.cpp
+++ main/main.cpp
@@ -1,25 +1,15 @@
-#include <stdio.h>
 extern "C" {
 #include "bsp/device.h"
 #include "bsp/power.h"
 #include "bsp_lvgl.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "hal/lcd_types.h"
 #include "nvs_flash.h"
 }
 #include "Meshtastic.h"
 
 // Constants
 static char const TAG[] = "main";
-
-// Global variables
-static esp_lcd_panel_handle_t       display_lcd_panel    = NULL;
-static esp_lcd_panel_io_handle_t    display_lcd_panel_io = NULL;
-static size_t                       display_h_res        = 0;
-static size_t                       display_v_res        = 0;
-static lcd_color_rgb_pixel_format_t display_color_format;
-static QueueHandle_t                input_event_queue = NULL;
 
 extern "C" void app_main(void) {
     // Start the GPIO interrupt service
