7d5a99484ec120e869c5bb97f6d33c02524e9ec0
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2025-05-24 19:20:29 +0200

    feat: add display powersaving

--- main/BadgeBspDisplay.cpp
+++ main/BadgeBspDisplay.cpp
@@ -2,7 +2,6 @@
 #include "freertos/idf_additions.h"
 extern "C" {
 #include "bsp/display.h"
-#include "bsp/input.h"
 #include "bsp_lvgl.h"
 }
 #include "display/lv_display.h"
@@ -15,11 +14,15 @@ static esp_lcd_panel_io_handle_t    display_lcd_panel_io;
 static size_t                       display_h_res;
 static size_t                       display_v_res;
 static lcd_color_rgb_pixel_format_t display_color_format;
-static QueueHandle_t                input_event_queue;
 
 BadgeBspDisplay* BadgeBspDisplay::bsp_display = NULL;
 
-BadgeBspDisplay::BadgeBspDisplay(uint16_t width, uint16_t height) : DisplayDriver(width, height) {
+BadgeBspDisplay::BadgeBspDisplay(uint16_t width, uint16_t height)
+    : DisplayDriver(width, height),
+      screen_timeout_ms(defaultScreenTimeout),
+      last_brightness(defaultBrightness),
+      powersaving(false) {
+    setBrightness(defaultBrightness);
 }
 
 BadgeBspDisplay& BadgeBspDisplay::create(QueueHandle_t input_event_queue) {
@@ -52,4 +55,49 @@ void BadgeBspDisplay::init(DeviceGUI* gui) {
     display      = lv_display_get_default();
 }
 
-void BadgeBspDisplay::task_handler(void) {};
+void BadgeBspDisplay::task_handler(void) {
+    if ((screen_timeout_ms > 0 && lv_display_get_inactive_time(NULL) > screen_timeout_ms) || powersaving ||
+        (DisplayDriver::view->isScreenLocked())) {
+        if (!powersaving) {
+            // dim display brightness slowly down
+            uint8_t brightness;
+            bsp_display_get_backlight_brightness(&brightness);
+            if (brightness > 1) {
+                bsp_display_set_backlight_brightness(brightness - 1);
+            } else {
+                ILOG_INFO("enter powersave");
+                DisplayDriver::view->screenSaving(true);
+                powersaving = true;
+            }
+        }
+    }
+    if (powersaving) {
+        int pin_int = -1;
+        if ((pin_int >= 0 && DisplayDriver::view->sleep(pin_int)) ||
+            (screen_timeout_ms + 50 > lv_display_get_inactive_time(NULL) && !DisplayDriver::view->isScreenLocked())) {
+            // delay(2); // let the CPU finish to restore all register in case of light sleep
+            // // woke up by touch or button
+            ILOG_INFO("leaving powersave");
+            powersaving = false;
+            DisplayDriver::view->triggerHeartbeat();
+            setBrightness(last_brightness);
+            DisplayDriver::view->screenSaving(false);
+            lv_display_trigger_activity(NULL);
+        } else {
+            // we woke up due to e.g. serial traffic (or sleep() simply not implemented)
+            // continue with processing loop and enter sleep() again next round
+        }
+    }
+};
+
+uint8_t BadgeBspDisplay::getBrightness() {
+    uint8_t percentage;
+    bsp_display_get_backlight_brightness(&percentage);
+    uint8_t value = (((uint16_t)percentage * 255) / 100);
+    return value;
+}
+
+void BadgeBspDisplay::setBrightness(uint8_t brightness) {
+    uint8_t percentage = ((uint16_t)brightness * 100) / 255;
+    bsp_display_set_backlight_brightness(percentage);
+}
--- main/BadgeBspDisplay.h
+++ main/BadgeBspDisplay.h
@@ -4,6 +4,9 @@
 #include "freertos/FreeRTOS.h"
 #include "graphics/driver/DisplayDriver.h"
 
+constexpr uint32_t defaultScreenTimeout = 30 * 1000;
+constexpr uint32_t defaultBrightness    = 255;
+
 class BadgeBspDisplay : public DisplayDriver {
    public:
     static BadgeBspDisplay& create(QueueHandle_t input_event_queue);
@@ -11,9 +14,28 @@ class BadgeBspDisplay : public DisplayDriver {
     bool                    hasButton(void) override {
         return true;
     };
+    bool hasLight(void) override {
+        return true;
+    }
     void task_handler(void) override;
+    bool isPowersaving(void) override {
+        return powersaving;
+    }
+
+    uint8_t getBrightness() override;
+    void    setBrightness(uint8_t brightness) override;
+
+    uint16_t getScreenTimeout() override {
+        return screen_timeout_ms / 1000;
+    }
+    void setScreenTimeout(uint16_t timeout) override {
+        screen_timeout_ms = timeout * 1000;
+    };
 
    private:
     BadgeBspDisplay(uint16_t width, uint16_t height);
     static BadgeBspDisplay* bsp_display;
+    uint32_t                screen_timeout_ms;
+    uint32_t                last_brightness;
+    bool                    powersaving;
 };
